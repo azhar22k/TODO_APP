503225e78ceadb7c44857265a68a22b506e0436e
Azhar Khan <azhar.khan@peak.bi>
2017-11-08 14:24:44 +0530

    Minor change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import Main from './store/store';
 
-
 console.log('THIS IS THE ROOT ELEMENT');
 ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
