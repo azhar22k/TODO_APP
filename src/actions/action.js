43481f3b5684b7e5befc04c1117da9b87f11d220
Azhar Khan <azhar.khan@peak.bi>
2017-11-09 12:19:05 +0530

    Added new actions

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,9 +1,10 @@
-let nextTodoId = 0;
+let nextTodoId = 1;
 
-export function addToDo(text) {
+export function addToDo(title = nextTodoId, text) {
+	++nextTodoId;
 	return {
 		type: 'UPDATE_LIST',
-		title: 'Note: '+(++nextTodoId),
+		title: 'Note: '+title,
 		text: text
 	};
 }
@@ -11,6 +12,22 @@ export function addToDo(text) {
 export function deleteTaskList(index) {
 	return {
 		type: 'DELETE_TASK_LIST',
+		index: index
+	};
+}
+
+export function updateButtonName() {
+	return {
+		type: 'UPDATE_BUTTON_NAME'
+	};
+}
+
+export function updateTaskList(title = nextTodoId, text, index) {
+	++nextTodoId;
+	return {
+		type: 'UPDATE_TASK',
+		title: 'Note: '+title,
+		text: text,
 		index: index
 	};
 }
